d254f2f608cb3062a0689a93cab431d37db8237c
Kevin Hubbard <dgkallday98@gmail.com>
2019-10-03 16:25:44 -0400

    inital bilaExt commit

---
+++ bilaext.js
@@ -0,0 +1,6 @@
+document.getElementsByClassName('right_side')[0].remove();
+document.getElementById('gamesinfo').remove();
+document.getElementById('responsive').remove();
+
+
+setTimeout(function(){ document.getElementsByClassName('cc-window')[0].remove() }, 3000);
